254f36fdba03f21eecb02e3421be1db1fc7e0946
tavothuong <vanlongpro.info@gmail.com>
2020-06-05 11:34:06 +0700

    first commit

---
+++ products/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+	title		= models.CharField(max_length=100) # max_length is required
+	description = models.TextField(blank=True, null=True)
+	price		= models.DecimalField(decimal_places=2, max_digits=10000)
+	summary		= models.TextField()
+	featured	= models.BooleanField(default=True)
